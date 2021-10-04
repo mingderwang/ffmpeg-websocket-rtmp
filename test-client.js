c8deacf25df3ad487687491298a468b9bf33f1cc
Ming-der Wang <mingderwang@gmail.com>
2021-10-04 15:58:03 +0800

    ok for yarn test

---
+++ test-client.js
@@ -0,0 +1,15 @@
+var ws = require('websocket-stream')
+var test = require('tape')
+var Buffer = require('safe-buffer').Buffer
+
+test('echo works', function(t) {
+  var stream = ws('ws://localhost:3000', { binary: true })
+  stream.on('data', function(o) {
+    t.ok(Buffer.isBuffer(o), 'is buffer')
+    t.equal(o.toString(), 'hello', 'got hello back')
+    stream.destroy()
+    t.end()
+  })
+  stream.write(Buffer.from('hello'))
+  stream.write('lolololololololololok')
+})
